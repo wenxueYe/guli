91d39d009a8b0c5ee6920dc3276fff330d0b0cca
yewenxue <yewenxuewy@163.com>
2022-01-15 01:00:46 +0800

    基础模板v1.2

--- guli-order/src/main/java/org/yewenxue/order/GuliOrderApplication.java
+++ guli-order/src/main/java/org/yewenxue/order/GuliOrderApplication.java
@@ -3,11 +3,15 @@ package org.yewenxue.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Time : 2022/1/15 0:07
+ * @Author : wenxue ye
+ * @File : GuliOrderApplication
+ * @Project : guli
+ */
 @SpringBootApplication
 public class GuliOrderApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(GuliOrderApplication.class, args);
+        SpringApplication.run(GuliOrderApplication.class,args);
     }
-
 }
